d7460c6100edc89e1a9a0baa0ff3d7f071761454
maxog <maxogloblia@gmail.com>
2024-11-05 17:23:26 +0700

    initial version of caesar cipher class

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,2 +1,34 @@
 public class CaesarCipher {
+    private CaesarCipher() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,«»\"':!? ";
+    private static final int ALPHABET_SIZE = ALPHABET.length();
+
+    public static String encrypt(String text, int key) {
+        return shiftText(text, key);
+    }
+
+    public static String decrypt(String text, int key) {
+        return shiftText(text, -key);
+    }
+
+    //Метод для зсуву символів рядку по алфавіту за визначеним значенням
+    private static String shiftText(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for (char character : text.toCharArray()) {
+            int index = ALPHABET.indexOf(character);
+
+            if (index != -1) {  //Якщо символ є у вказаному алфавіті - зсуваємо
+                int shiftedIndex = (index + shift + ALPHABET_SIZE) % ALPHABET_SIZE;
+                result.append(ALPHABET.charAt(shiftedIndex));
+            } else {
+                result.append(character);
+            }
+        }
+
+        return result.toString();
+    }
 }
